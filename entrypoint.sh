34d05634008dba4cd23bddabae982c0a1af58cb3
tariq khasawneh <tariq.khasawneh@Jt.JtGroup>
2022-01-09 20:35:06 +0200

    third

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,14 @@ set -e
 
 if [ -z "$GCP_STORAGE_BUCKET" ]; then
   echo "GCP_STORAGE_BUCKET is not set. Quitting."
+  read -p "Press any key to resume ..."
   exit 1
 fi
 
 if [ -z "$GCP_SERVICE_ACCOUNT_KEY_FILE" ]; then
   echo "GCP_SERVICE_ACCOUNT_KEY_FILE is not set. Quitting."
+  read -p "Press any key to resume ..."
+
   exit 1
 fi
 
@@ -19,3 +22,5 @@ sh -c "gsutil -m rsync $* -r ${SOURCE_DIR} gs://${GCP_STORAGE_BUCKET}/${DEST_DIR
 
 gcloud auth revoke --all
 shred -zvu -n 5 json_file.json
+
+
